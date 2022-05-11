faf11522df8c066dc3916e400e6439c777f87e6c
Dikshant Gupta <dikshant2210@gmail.com>
2022-05-11 08:07:22 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "vector"
+
+using namespace std;
+
+struct Point {
+    double x;
+};
+
+void g_h_filter(const vector<Point>& data, Point x0, double dx, double g, double h, double dt) {
+
+}
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
